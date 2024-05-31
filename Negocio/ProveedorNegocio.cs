3bb829bad1cfbaa5142f0eea2785b61e48f6809c
Mauro Asselborn <mauasselborn@gmail.com>
2024-05-31 18:46:47 -0300

    Se agregan Clases Proveedor y Stock

--- Negocio/ProveedorNegocio.cs
+++ Negocio/ProveedorNegocio.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dominio
+namespace Negocio
 {
-    public class Class1
+    public class ProveedorNegocio
     {
     }
 }

af25551df9e2f234105f6ddf33f2b79413ed6d88
Mauro Asselborn <mauasselborn@gmail.com>
2024-06-04 13:59:25 -0300

    Se modifican Scripts de SQL, Listar Categorias y Proveedores

---
+++ DB/Insercion de Datos.sql
@@ -0,0 +1,33 @@
+-- Insert Marcas
+INSERT INTO Marcas(Descripcion) VALUES('Samsung') 
+INSERT INTO Marcas(Descripcion) VALUES('LG') 
+INSERT INTO Marcas(Descripcion) VALUES('Dell') 
+INSERT INTO Marcas(Descripcion) VALUES('Motorola') 
+-- Insert Categorias
+INSERT INTO Categorias(Descripcion) VALUES('Celular') 
+INSERT INTO Categorias(Descripcion) VALUES('Parlante') 
+INSERT INTO Categorias(Descripcion) VALUES('Monitor') 
+INSERT INTO Categorias(Descripcion) VALUES('Notebook')
+
+
+-- Insert Proveedores
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Tana', 'Lajo', '03-243-5214', '(733) 8618677', 'Room 1691', 'tbalaizot0@spotify.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Serene', 'Eazzy', '09-533-2224', '(833) 1848596', 'Apt 625', 'sgeockle1@devhub.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Berkly', 'Zoomlounge', '04-417-5862', '(383) 3473499', 'Apt 1649', 'bpentony2@cnn.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Millicent', 'Twiyo', '32-763-0668', '(809) 3937469', 'Suite 32', 'mtrowler3@slideshare.net');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Antonius', 'Bubbletube', '57-413-6236', '(954) 8132349', 'Apt 110', 'amidgley4@ihg.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Linda', 'Jabbersphere', '65-058-5562', '(947) 8337499', 'Apt 1766', 'lmatuszyk5@jiathis.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Sheeree', 'Gevee', '76-920-6921', '(282) 2807593', '1st Floor', 'sangerstein6@pinterest.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Korry', 'Mybuzz', '65-052-5530', '(956) 3895492', 'Room 988', 'ksmallpeace7@columbia.edu');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Beaufort', 'Geba', '75-869-3691', '(570) 3130389', 'Apt 1556', 'btacker8@qq.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Jason', 'Zoomdog', '32-928-7667', '(414) 7726609', 'Suite 61', 'jrawdall9@g.co');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Lisha', 'Kanoodle', '61-302-0965', '(757) 1210205', 'PO Box 26349', 'lrowa@studiopress.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Julee', 'Kamba', '02-337-7139', '(270) 4175284', 'PO Box 80327', 'jvaughtonb@ning.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Grantley', 'Voonte', '88-880-8373', '(596) 7593764', 'Suite 70', 'gmacnockaterc@delicious.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Siana', 'Janyx', '07-768-4975', '(336) 6360081', 'Apt 1775', 'sneljesd@clickbank.net');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Mathew', 'Youfeed', '24-699-8433', '(530) 9936070', 'Room 1524', 'mgersone@shinystat.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Morie', 'Youfeed', '96-783-5467', '(983) 3105246', '8th Floor', 'mjackettf@harvard.edu');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Booth', 'Browsecat', '80-910-5541', '(215) 6054778', 'Apt 749', 'bferronig@multiply.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Roobbie', 'Gigazoom', '57-102-6371', '(797) 6100896', 'Apt 811', 'rivanonkoh@multiply.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Scott', 'Latz', '93-405-6705', '(575) 9312777', 'Room 10', 'scoopei@naver.com');
+insert into Proveedores (Nombre, Empresa, Cuit, Telefono, Direccion, Email) values ('Jacquelynn', 'Quatz', '70-209-1809', '(374) 5821829', 'Room 1350', 'jiacovaccioj@meetup.com');

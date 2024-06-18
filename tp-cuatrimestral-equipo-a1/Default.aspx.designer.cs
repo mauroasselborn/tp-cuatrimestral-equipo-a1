fbc1741409c079457591dfe337bdc52135dfaad1
danydf1 <49538985+danydf1@users.noreply.github.com>
2024-06-18 20:15:04 -0300

    Login  y encriptacion

--- tp-cuatrimestral-equipo-a1/Default.aspx.designer.cs
+++ tp-cuatrimestral-equipo-a1/Default.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace tp_cuatrimestral_equipo_a1
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control inputEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputEmail;
+
+        /// <summary>
+        /// Control inputPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inputPassword;
+
+        /// <summary>
         /// Control btnLogin.
         /// </summary>
         /// <remarks>

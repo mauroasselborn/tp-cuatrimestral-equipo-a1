20e2efb089000dafc54369cb8d3115a387a8f042
Mauro Asselborn <mauasselborn@gmail.com>
2024-06-20 16:28:38 -0300

    ...

---
+++ tp-cuatrimestral-equipo-a1/DashboardUsr.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterUsr.Master" AutoEventWireup="true" CodeBehind="DashboardUsr.aspx.cs" Inherits="tp_cuatrimestral_equipo_a1.DashboardUsr" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <title>Dashboard Usuario</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <img class="img-fluid img-fondo" src="Img/TecnoStore.png" alt="Alternate Text" />
+</asp:Content>
---
+++ tp-cuatrimestral-equipo-a1/DashboardUsr.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp_cuatrimestral_equipo_a1
+{
+
+
+    public partial class DashboardUsr
+    {
+    }
+}

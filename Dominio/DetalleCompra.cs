8a3f344b3d019dac043262387e04042bdc01e86a
Mauro Asselborn <mauasselborn@gmail.com>
2024-06-25 18:09:13 -0300

    Clase DetalleCompra, Compra y cambios menores

---
+++ Dominio/DetalleCompra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class DetalleCompra
+    {
+        public Articulo Articulo { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        public DetalleCompra(Articulo articulo, int cantidad, decimal precioUnitario)
+        {
+            Articulo = articulo;
+            Cantidad = cantidad;
+            PrecioUnitario = precioUnitario;
+        }
+
+        public decimal Subtotal
+        {
+            get { return Cantidad * PrecioUnitario; }
+        }
+    }
+}
